fce7d15641010f20002ea7ca8de92c2102a859fb
Harryharran <harielumalai856@gmail.com>
2024-04-28 02:48:31 +0530

    new change

--- src/Component/Scroll.js
+++ src/Component/Scroll.js
@@ -3,7 +3,7 @@ import React from "react";
 function scroll (props){
     // console.log(props);
   return (
-    <div style={{overflowY:'scroll', border:'5px solid grey', height:'550px' }}>
+    <div style={{overflowY:'scroll', border:'5px solid grey', height:'750px' }}>
     {props.children}
     </div>
 )
